f40a0f4e2e5108bc7d1860bd3a9775961502ada2
Harry Wang <hyw2@duke.edu>
2018-04-27 15:28:52 -0400

    adding in donations dataset

--- finalApp/src/App.vue
+++ finalApp/src/App.vue
@@ -34,6 +34,7 @@
       <div v-else>
         <button v-on:click="load" class="btn btn-primary">Load Data</button>
         <button v-on:click="getLocation" class="btn btn-primary">Get Location</button>
+        <button v-on:click="ispDonations" class="btn btn-primary">Donations</button>
         <!-- <wheel></wheel> -->
       </div>
     </header>
@@ -44,6 +45,7 @@
 import * as d3 from 'd3'
 import request from 'request';
 import numerical from './components/numerical'
+import donations from './components/donations'
 
 var firebase = require('firebase');
 
@@ -58,115 +60,104 @@ var config = {
 firebase.initializeApp(config);
 
 export default {
-    name: 'App',
-    data() {
-        return {
-            legs: [],
-            categorical: [],
-            numerical: [],
-            numSum: [],
-            signedIn: false,
-            email: '',
-            password: ''
+  name: 'App',
+  data() {
+    return {
+      legs: [],
+      categorical: [],
+      numerical: [],
+      numSum: [],
+      signedIn: true,
+      email: '',
+      password: ''
+    }
+  },
+  components: {
+    // wheel
+    donations
+  },
+  mounted: function () {
+    var vm = this;
+    d3.csv("https://raw.githubusercontent.com/duke-compsci290-spring2018/final-project-team-56/zuzu/finalApp/data/legislators-current.csv", function(data) {
+      vm.legs.push(data);
+      console.log("done");
+    });
+  },
+  methods: {
+    load() {
+      var vm = this;
+      var cols = [];
+      var entries = d3.entries(vm.legs[0]);       // considering first entry only for column names
+      for (var k = 0; k < entries.length; k++) {
+        cols.push(entries[k].key); // column name
+        // how to interpret birthdays -- specific to legs
+        if (entries[k].key == "birthday") {
+          var birthday = Date.parse(entries[k].value);
+          var age = this.calculateAge(birthday);
+          entries[k].value = age;
         }
-    },
-    components: {
-        // wheel
-    },
-    mounted: function () {
-        var vm = this;
-        d3.csv("https://raw.githubusercontent.com/duke-compsci290-spring2018/final-project-team-56/zuzu/finalApp/data/legislators-current.csv", function(data) {
-            vm.legs.push(data);
-            console.log("done");
-        });
-    },
-    methods: {
-        load() {
-            var vm = this;
-            var cols = [];
-            var entries = d3.entries(vm.legs[0]);       // considering first entry only for column names
-            for (var k = 0; k < entries.length; k++) {
-                cols.push(entries[k].key); // column name
-                // how to interpret birthdays -- specific to legs
-                if (entries[k].key == "birthday") {
-                    var birthday = Date.parse(entries[k].value);
-                    var age = this.calculateAge(birthday);
-                    entries[k].value = age;
-                }
-                if (isNaN(entries[k].value)) {  // string --> categorical
-                    this.categorical.push(entries[k]);
-                } else {
-                    this.numerical.push(entries[k]);
-                }
-            }
-
-            for (var i = 0; i < this.categorical.length; i++) {
-                console.log("categorical var: " + this.categorical[i].key);
-            }
-            for (var i = 0; i < this.numerical.length; i++) {
-                console.log("numerical var: " + this.numerical[i].key);
-            }
-            for (var i = 0; i < this.legs.length; i++) {
-                this.legs[i].age = this.calculateAge(Date.parse(this.legs[i].birthday));
-                //console.log(this.calculateAge(Date.parse(this.legs[i].birthday)));
-            }
-            // TODO: missing data -> ignore column
-        },
-        getLocation() {
-            request("https://ipinfo.io", function(error, response, body) {
-                console.log(JSON.stringify(response, null, 4));
-                // console.log(JSON.parse(body));
-                // console.log("city " + response.city);
-                // console.log("state " + response.region)
-            }, "jsonp");
-        },
-        calculateAge(birthday) { // birthday is a date
-            var ageDifMs = Date.now() - birthday;
-            var ageDate = new Date(ageDifMs); // miliseconds from epoch
-            return Math.abs(ageDate.getUTCFullYear() - 1970);
-        },
-        generateNumericalSummary() {
-            var header;
-            var temp = {};
-            console.log(this.numerical.length);
-            for (var k = 0; k < this.numerical.length; k++) {
-                header = this.numerical[k];
-                console.log(header);
-                temp.header = d3.min(this.legs, function(d) { return d.header; });
-            }
-            //temp.age = ,//temp.height
-            // numSum is array of objects: mean, med, etc.
-            this.numSum.push(temp);
-            console.log(this.numSum);
+        if (isNaN(entries[k].value)) {  // string --> categorical
+          this.categorical.push(entries[k]);
+        } else {
+          this.numerical.push(entries[k]);
         }
       }
-      /*
-      for (var i = 0; i < categorical.length; i++) {
-      console.log("categorical var: " + categorical[i].key);
+
+      for (var i = 0; i < this.categorical.length; i++) {
+        console.log("categorical var: " + this.categorical[i].key);
+      }
+      for (var i = 0; i < this.numerical.length; i++) {
+        console.log("numerical var: " + this.numerical[i].key);
+      }
+      for (var i = 0; i < this.legs.length; i++) {
+        this.legs[i].age = this.calculateAge(Date.parse(this.legs[i].birthday));
+        //console.log(this.calculateAge(Date.parse(this.legs[i].birthday)));
+      }
+      // TODO: missing data -> ignore column
+    },
+    getLocation() {
+      // request("https://ipinfo.io", function(error, response, body) {
+      //   console.log(JSON.stringify(response, null, 4));
+      //   // console.log(JSON.parse(body));
+      //   // console.log("city " + response.city);
+      //   // console.log("state " + response.region)
+      // }, "jsonp");
+      //console.log(this.legs[0]);
+      //firebase.database().ref("legs").set(this.legs);
+    },
+    calculateAge(birthday) { // birthday is a date
+      var ageDifMs = Date.now() - birthday;
+      var ageDate = new Date(ageDifMs); // miliseconds from epoch
+      return Math.abs(ageDate.getUTCFullYear() - 1970);
+    },
+    generateNumericalSummary() {
+      var header;
+      var temp = {};
+      console.log(this.numerical.length);
+      for (var k = 0; k < this.numerical.length; k++) {
+        header = this.numerical[k];
+        console.log(header);
+        temp.header = d3.min(this.legs, function(d) { return d.header; });
+      }
+      //temp.age = ,//temp.height
+      // numSum is array of objects: mean, med, etc.
+      this.numSum.push(temp);
+      console.log(this.numSum);
     }
-    for (var i = 0; i < numerical.length; i++) {
-    console.log("numerical var: " + numerical[i].key);
-  }*/
-  for (var i = 0; i < this.legs.length; i++) {
-    this.legs[i].age = this.calculateAge(Date.parse(this.legs[i].birthday));
-    //console.log(this.calculateAge(Date.parse(this.legs[i].birthday)));
+  },
+  getLocation() {
+    request("https://ipinfo.io", function(error, response, body) {
+      console.log(JSON.stringify(response, null, 4));
+      // console.log(JSON.parse(body));
+      // console.log("city " + response.city);
+      // console.log("state " + response.region)
+    }, "jsonp");
+  },
+  calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday;
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
-  // TODO: missing data -> ignore column
-},
-getLocation() {
-  request("https://ipinfo.io", function(error, response, body) {
-    console.log(JSON.stringify(response, null, 4));
-    // console.log(JSON.parse(body));
-    // console.log("city " + response.city);
-    // console.log("state " + response.region)
-  }, "jsonp");
-},
-calculateAge(birthday) { // birthday is a date
-  var ageDifMs = Date.now() - birthday;
-  var ageDate = new Date(ageDifMs); // miliseconds from epoch
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
-}
 }
 </script>
 
